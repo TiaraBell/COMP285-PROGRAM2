6245e380846709aeeabf730831abc2cb697f1cf6
Tiara <tbell1@aggies.ncat.edu>
2019-10-16 17:26:01 -0400

    add test cases

--- MST/src/mst/TestRunner.java
+++ MST/src/mst/TestRunner.java
@@ -71,10 +71,82 @@ public class TestRunner {
         // Set the pruned graph weight
         final int prunedWeight1 = 51;
 		
-		/**
-		TODO: Add your own image, intensity weight and pruned weight for 3 to 5 interesting tests
-		Reminder: your test case graphs should be of size 4 by 4 or larger
-		**/
+	/**
+	TODO: Add your own image, intensity weight and pruned weight for 3 to 5 interesting tests
+	Reminder: your test case graphs should be of size 4 by 4 or larger
+	**/
+        
+        // Test Case 1 of 5
+
+        // Initialize the image
+        final int [][] image2 = new int[][] {
+                    { 0,  1,  0,  1},
+                    { 1,  0,  1,  0},
+                    { 0,  1,  0,  1},
+                    { 1,  0,  1,  0}
+                    };
+        // Set the intensity graph weight
+        final int intensityWeight2 = 24;
+        // Set the pruned graph weight
+        final int prunedWeight2 = 15;
+        
+        // Test Case 2 of 5
+
+        // Initialize the image
+        final int [][] image3 = new int[][] {
+                    {   1,  56, 19, 178},
+                    {  76,  20, 81, 132},
+                    { 202,  73, 34,   2},
+                    {  68, 156,  0,  49}
+                    };
+        // Set the intensity graph weight
+        final int intensityWeight3 = 24;
+        // Set the pruned graph weight
+        final int prunedWeight3 = 15;
+        
+        // Test Case 3 of 5
+
+        // Initialize the image
+        final int [][] image4 = new int[][] {
+                    { 1, 2, 3, 4},
+                    { 2, 2, 3, 4},
+                    { 3, 3, 3, 4},
+                    { 4, 4, 4, 4}
+                    };
+        // Set the intensity graph weight
+        final int intensityWeight4 = 24;
+        // Set the pruned graph weight
+        final int prunedWeight4 = 15;
+        
+        // Test Case 4 of 5
+
+        // Initialize the image
+        final int [][] image5 = new int[][] {
+                    { 1, 2, 3, 5},
+                    { 2, 1, 2, 4},
+                    { 3, 0, 1, 3},
+                    { 5, 2, 3, 5}
+                    };
+        // Set the intensity graph weight
+        final int intensityWeight5 = 24;
+        // Set the pruned graph weight
+        final int prunedWeight5 = 15;
+        
+        // Test Case 5 of 5
+
+        // Initialize the image
+        final int [][] image6 = new int[][] {
+                    { 10, 11, 12, 2},
+                    { 11, 10, 11, 3},
+                    { 12, 11, 12, 5},
+                    { 4,   6,  7, 6}
+                    };
+        // Set the intensity graph weight
+        final int intensityWeight6 = 24;
+        // Set the pruned graph weight
+        final int prunedWeight6 = 15;
+
+        
 		
         System.out.println("----- Running Tests ------");
 
@@ -83,6 +155,12 @@ public class TestRunner {
         // Create and add test case based on specified image and intensity and pruned graph weights
         tests.add(new TestCase(image0, intensityWeight0, prunedWeight0));
         tests.add(new TestCase(image1, intensityWeight1, prunedWeight1));
+        tests.add(new TestCase(image2, intensityWeight2, prunedWeight2));
+        tests.add(new TestCase(image3, intensityWeight3, prunedWeight3));
+        tests.add(new TestCase(image4, intensityWeight4, prunedWeight4));
+
+
+
 
 		/**
 		TODO: Create 3 to 5 test case objects using the image, intensity weight and pruned weight you created and add them to "tests"
